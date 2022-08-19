948544ed34211af711c1ad4bd8a658ab4e089998
AndreeaBrumaru <andreea.brumaru@metro.digital>
2022-08-19 09:11:32 +0300

    Initial commit

---
+++ src/main/java/com/example/librarysptingapplication/LibrarySptingApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarysptingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySptingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySptingApplication.class, args);
+	}
+
+}
